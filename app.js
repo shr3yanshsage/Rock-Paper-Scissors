336c7e5c0ca9d7001299113c52a255506f535bae
shr3yanshsage <iampurusingh07@gmail.come.com>
2024-04-27 14:43:12 +0530

    added files

---
+++ app.js
@@ -0,0 +1,15 @@
+
+
+import { Component } from 'react';
+import './App.css';
+import Game from './components/Game';
+
+class App extends Component{ 
+	render(){
+	return (
+	<Game/>
+	);
+}
+}
+
+export default App;
